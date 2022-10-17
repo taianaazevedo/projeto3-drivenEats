54fa6a49da856197a8e8c414cf7e34e367686821
taianaazevedo <taianazev@gmail.com>
2022-10-16 21:03:14 -0300

    botoes habilitados

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+let pratoEscolhido;
+
 function escolherPrato(botao){
     const botaoSelecionado = document.querySelector('.pratos .selecionado');
     if (botaoSelecionado !== null){
         botaoSelecionado.classList.remove('selecionado');
     }
     botao.classList.toggle('selecionado');
+
+    pratoEscolhido = botao.innerHTML;
+
+    fecharPedido()
 }
+
+let bebidaEscolhida;
 
 function escolherBebida(botao2){
     const botaoSelecionado2 = document.querySelector('.bebidas .selecionado')
@@ -12,7 +20,13 @@ function escolherBebida(botao2){
         botaoSelecionado2.classList.remove('selecionado');
     }
     botao2.classList.toggle('selecionado');
+
+    bebidaEscolhida = botao2.innerHTML;
+
+    fecharPedido()
 }
+
+let doceEscolhido;
 
 function escolherDoce(botao3){
     const botaoSelecionado3 = document.querySelector('.sobremesa .selecionado')
@@ -20,9 +34,20 @@ function escolherDoce(botao3){
         botaoSelecionado3.classList.remove('selecionado');
     }
     botao3.classList.toggle('selecionado');
+
+    doceEscolhido = botao3.innerHTML;
+
+    fecharPedido()
 }
 
 function fecharPedido(){
-    alert('oi')
+    if(pratoEscolhido !== undefined && bebidaEscolhida !== undefined && doceEscolhido !== undefined){
+                const botaoFechar = document.querySelector('.botao');
+                botaoFechar.classList.add('escondido'); 
+                botaoFechar.innerHTML = "Fechar Pedido";
+                    
+        }
 }
+
+
 
